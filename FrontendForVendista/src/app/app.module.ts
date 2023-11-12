8a8a24b2dd5d42b2cea7f227bde7fa28470dc534
on1x22 <on1x22@mail.ru>
2023-11-12 21:46:51 +0400

    Refactoring

--- FrontendForVendista/src/app/app.module.ts
+++ FrontendForVendista/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontendForVendista/src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, Inject } from '@angular/core';
+
+@Component({
+  selector: 'header-app',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent {
+
+}
