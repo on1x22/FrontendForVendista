e8da7079fc63ede4cc30109d7078d6856b59ce54
on1x22 <on1x22@mail.ru>
2023-12-12 22:19:40 +0400

    Completed Owners page

--- FrontendForVendista/src/app/owners/owners.component.ts
+++ FrontendForVendista/src/app/owners/owners.component.ts
@@ -62,8 +62,11 @@ export class OwnersComponent {
     if (this.ownerInfo == null)
       return;
 
+    this.ownerInfo.modify_time = new Date();
     let responce;
-    await this.httpClient.put<OwnerInfo>(`/owners/${this.selectedOwnerId}?token=f0d17d3cae184917802e2ef2`, this.ownerInfo)
+    //await this.httpClient.put<OwnerInfo>(`/owners/${this.selectedOwnerId}?token=f0d17d3cae184917802e2ef2`, this.ownerInfo)
+    //  .subscribe(result => { responce = result; });
+    await this.httpClient.put<OwnerInfo>(`/owners?token=f0d17d3cae184917802e2ef2`, this.ownerInfo)
       .subscribe(result => { responce = result; });
 
     this.editModeIsOff();
