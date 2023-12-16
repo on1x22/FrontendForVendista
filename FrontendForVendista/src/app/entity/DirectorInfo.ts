90fdf24b32c2c0f857179cb51bbf6530efd1d690
on1x22 <on1x22@mail.ru>
2023-12-16 16:04:47 +0400

    Added modal window to Owners page
     Added modal widow with director information to Owner page

---
+++ FrontendForVendista/src/app/entity/DirectorInfo.ts
@@ -0,0 +1,24 @@
+export class DirectorInfo {
+  name!: string;
+  surname!: string;
+  patronym!: string;
+  birth_date!: string;
+  birth_place!: string;
+  citizenship!: string;
+  type!: string;
+  series!: string;
+  number!: string;
+  date_of_issue!: string;
+  issued_by!: string;
+  department_code!: string;
+  zip_code!: string;
+  region!: string;
+  settlement_type!: string;
+  location!: string;
+  street_type!: string;
+  street!: string;
+  building_number!: string;
+  block_number!: string;
+  room_number!: string;
+  full_address!: string;
+}
